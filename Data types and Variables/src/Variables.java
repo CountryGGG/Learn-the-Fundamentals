af9fa7106da839e55f7b208aa1a37f70e51de624
CountryGGG <u202121858@upc.edu.pe>
2024-12-11 02:19:28 -0500

    feat: add Variables
    Variables en Java

---
+++ Data types and Variables/src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args){
+        //Variables
+        int edad = 33;
+        double precio = 300.40;
+        boolean disponible = true; //true/false
+        char genero;
+        genero = 'M'; //M-Masculino / F-Femenino
+
+        //Acceder a las variables
+        System.out.println(edad);
+        //Modificamos el valor
+        edad = 35;
+        System.out.println(edad);
+        //Imprimimmos el resto de las variables
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+    }
+}
