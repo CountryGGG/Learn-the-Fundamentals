89717594c96257d30e17526af5121c16f9559e9b
CountryGGG <u202121858@upc.edu.pe>
2024-12-11 05:29:42 -0500

    feat: Add Detalle Persona Ejercicio

---
+++ Data types and Variables/src/DetallePersona.java
@@ -0,0 +1,15 @@
+public class DetallePersona {
+    public static void main(String[] args) {
+        //Detalles de una Persona
+        String nombreCompleto = "Juanito Alimaña";
+        int edad = 33;
+        float altura = 1.88F;
+        String paisOrigen = "Perú";
+        char estadoCivil = 'S';
+        System.out.println(nombreCompleto);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(paisOrigen);
+        System.out.println(estadoCivil);
+    }
+}
